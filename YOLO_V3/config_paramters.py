7821683bd8f5a61b74a111bf799121743996640d
next <364574103@qq.com>
2025-10-13 13:11:05 +0800

    1013

--- YOLO_V3/config_paramters.py
+++ YOLO_V3/config_paramters.py
@@ -5,10 +5,12 @@
 # 数据集参数配置
 IMAGE_SIZE = 640
 GRID_SIZE = 32
+RATIO = 0.3  # 验证集与训练集的划分比例
 
 
 # 训练参数配置
 BATCH_SIZE = 4
+NUM_WORKERS = 4
 
 # LEARNING_RATE_INIT = 0.01  # initial learning rate (SGD=1E-2, Adam=1E-3)
 # LEARNING_RATE_FINAL = 0.01  # final OneCycleLR learning rate (LEARNING_RATE_INIT * LEARNING_RATE_FINAL)
--- YOLO_V3/datasets.py
+++ YOLO_V3/datasets.py
@@ -2,11 +2,13 @@
     Yolov3 的数据集构建程序文件
 """
 import os.path
-
 import config_paramters
 import config_path
 import utils
 from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset
+from tqdm import tqdm
 
 
 # 数据加载和预处理
@@ -39,6 +41,11 @@ class COCODataset(Dataset):
         print(self.images[idx], self.labels[idx])
 
 
+        return idx
+
+
+
+
 
 
 
@@ -54,7 +61,67 @@ def main():
     dataInfo = utils.create_data_path(config_path.DATASETS_YAML)
 
     # 创建数据集
-    trainDataset = COCODataset(dataInfo['path'], config_paramters.IMAGE_SIZE)
+    fullDatasets = COCODataset(dataInfo['path'], config_paramters.IMAGE_SIZE)
+
+
+    # 划分索引，整理出训练集与验证集
+    indices = list(range(len(fullDatasets)))
+    trainIndices, valIndices = train_test_split(
+        indices,
+        test_size=config_paramters.RATIO,   # 作为验证集的比例
+        random_state=42,    # 设定随机数种子 使得每次分配的整体集合是一致的
+        shuffle=True        # 设置是否打乱
+    )
+
+    trainDatasets = Subset(
+        COCODataset(dataInfo['path'], config_paramters.IMAGE_SIZE),
+        trainIndices
+    )
+
+    valDatasets = Subset(
+        COCODataset(dataInfo['path'], config_paramters.IMAGE_SIZE),
+        valIndices
+    )
+
+    # 创建数据集加载器
+    trainDataLoader = DataLoader(
+        trainDatasets,
+        batch_size=config_paramters.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config_paramters.NUM_WORKERS,
+        drop_last=True  # 不保留最后一个不完整批次
+    )
+
+    valDataLoader = DataLoader(
+        valDatasets,
+        batch_size=config_paramters.BATCH_SIZE,
+        shuffle=False,
+        num_workers=config_paramters.NUM_WORKERS,
+        drop_last=True  # 不保留最后一个不完整批次
+    )
+
+
+    os.makedirs('visualization', exist_ok=True)
+    # 可视化一下加载的数据集
+    with tqdm(total=len(trainDataLoader)+len(valDataLoader), desc="数据集可视化中") as pbarDataloader:
+
+        for batchIndex in enumerate(trainDataLoader):
+            for i in range(config_paramters.BATCH_SIZE):
+                print(f'batchIndex: {batchIndex}, i: {i}')
+            pbarDataloader.update(1)
+
+
+        # # 训练集
+        # for batchIndex, (originalData, augmentationData, targets) in enumerate(trainDataLoader):
+        #     for i in range(config_paramters.BATCH_SIZE):
+        #         print(targets.shape)
+        #     pbarDataloader.update(1)
+
+    #     # 验证集
+    #     for batchIndex, (originalData, augmentationData, targets) in enumerate(valDataLoader):
+    #         for i in range(config_parameter.BATCH_SIZE):
+    #             print(targets.shape)
+    #         pbarDataloader.update(1)
 
 
 
