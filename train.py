97d6dc782fb2353485025d1cc3a7bfad49035742
next <364574103@qq.com>
2025-10-17 21:40:53 +0800

    1017

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ def YOLOV3_VOC2007_TRAIN_MAIN():
     optimizer = optim.SGD(model.parameters(), lr=config_parameter.LEARNING_RATE, momentum=config_parameter.MOMENTUM, weight_decay=config_parameter.WEIGHT_DECAY)
 
     # 学习率调度器  多步学习率调度器   milestones=[50, 80]: 触发学习率调整的epoch位置   gamma=0.1: 学习率衰减的乘数因子
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 80], gamma=0.1)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 50, 80, 150, 190], gamma=0.1)
 
     # 创建损失函数
     criterion = YOLO_V3.loss.YOLOv3Loss()
