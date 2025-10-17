9a8781f3879192ee28e07c167831dcd3582d4abc
next <364574103@qq.com>
2025-10-17 13:15:47 +0800

    1017

---
+++ config_path.py
@@ -0,0 +1,21 @@
+
+"""
+    复现项目时用到的相关路径路径配置
+"""
+import os
+
+
+
+
+
+
+# 数据集的路径配置
+DATA_PATH = '../YOLO_data'
+TRAIN_DATA_PATH = os.path.join(DATA_PATH, 'VOC2007_train_and_val')
+TEST_DATA_PATH = os.path.join(DATA_PATH, 'VOC2007_test')
+
+
+
+
+
+
