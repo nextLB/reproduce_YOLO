cf5ea5b66975074f08b14c6625bddf7e3d1455e4
next <364574103@qq.com>
2025-10-19 22:11:26 +0800

    1019

--- YOLO_V4/models.py
+++ YOLO_V4/models.py
@@ -20,7 +20,45 @@ import math
 
 # Mish激活函数
 class Mish(nn.Module):
+    def __init__(self):
+        super(Mish, self).__init__()
+
+    def forward(self, x):
+        return x * torch.tanh(F.softplus(x))
 
 
+# 卷积块： conv + BN + Mish
+class ConvBnMish(nn.Module):
+    def __init__(self, inChannels, outChannels, kernelSize, stride=1, padding=0):
+        super(ConvBnMish, self).__init__()
+        self.conv = nn.Conv2d(inChannels, outChannels, kernelSize, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(outChannels)
+        self.mish = Mish()
 
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.mish(x)
+        return x
+
+
+# 残差块
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = ConvBnMish(channels, channels, 1)
+        self.conv2 = ConvBnMish(channels, channels, 3, padding=1)
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x + residual
+
+
+# CSP块
+class CspBlock(nn.Module):
+    def __init__(self, inChannels, outChannels, numBlocks):
+        super(CspBlock, self).__init__()
+        self.downSample = ConvBnMish(inChannels, outChannels, )
 
